df53ecf8a3b1155b3143b448a0e2bad3f2e0d28b
automatethem <108699325+automatethem@users.noreply.github.com>
2022-09-15 12:58:44 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import *
+
+LONG_DESC = """
+This is pytorch utilities
+"""
+
+setup(name='pytorch_utils',
+	  version='0.0.1',
+	  description='Pytorch utilities',
+	  long_description=LONG_DESC,
+	  author='Sang Ki Kwon',
+	  url='https://github.com/automatethem/pytorch_utils',
+	  install_requires=['pytorch', 'torchsummaryX'],
+	  author_email='automatethem@gmail.com',
+	  license='MIT',
+	  packages=find_packages(),
+	  zip_safe=False)
