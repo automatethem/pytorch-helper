4a179f2bdb93650aed13c60500142fcb1b3d8d32
automatethem <108699325+automatethem@users.noreply.github.com>
2022-09-17 22:52:40 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def requirements():
 
 setuptools.setup(
 	name='pytorch-helper',
-	version='0.0.7',
+	version='0.0.8',
 	description='Pytorch helper',
 	long_description=open('README.md').read(),
 	author='Sang Ki Kwon',
