b63be84bad8211df55f66a003730bbe2e23cc05c
automatethem <108699325+automatethem@users.noreply.github.com>
2022-11-22 19:55:40 +0900

    Update model.py

--- pytorch_helper/model.py
+++ pytorch_helper/model.py
@@ -28,14 +28,17 @@ class DotProduct(torch.nn.Module):
 class RNNLastOutput(torch.nn.Module):
     def __init__(self, input_size=1, hidden_size=32, batch_first=True):
         super().__init__()
+        #print(input_size) #300
+        #print(hidden_size) #32
         self.rnn = torch.nn.RNN(input_size=input_size, hidden_size=hidden_size, batch_first=batch_first)
 
     def forward(self, x):
-        #print(x.shape) #torch.Size([2, 7, 1])
-        output, (hidden, cell) = self.rnn(x)
-        #print(output.shape) #torch.Size([2, 7, 32]) #리턴값은 (배치 크기, 시퀀스 길이, 은닉 상태의 크기)
+        #print(x.shape) #torch.Size([32, 54, 300])
+        output, hidden = self.rnn(x)
+        #print(output.shape) #torch.Size([32, 54, 32]) #리턴값은 (배치 크기, 시퀀스 길이, 은닉 상태의 크기)
+        #print(hidden.shape) #torch.Size([1, 32, 32]) #containing the final hidden state for each element in the batch.
         x = output[:,-1]
-        #print(x.shape) #torch.Size([2, 32]) #(배치 크기, 은닉 상태의 크기)의 텐서로 크기가 변경됨. 즉, 마지막 time-step의 은닉 상태만 가져온다.
+        #print(x.shape) #torch.Size([32, 32]) #(배치 크기, 은닉 상태의 크기)의 텐서로 크기가 변경됨. 즉, 마지막 time-step의 은닉 상태만 가져온다.
         return x
     
 class LSTMLastOutput(torch.nn.Module):
