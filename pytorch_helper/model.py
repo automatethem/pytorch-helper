7701e6f7e4e2c5f49200b244678693df77a1db92
automatethem <108699325+automatethem@users.noreply.github.com>
2023-01-12 14:48:24 +0900

    Update model.py

--- pytorch_helper/model.py
+++ pytorch_helper/model.py
@@ -1,5 +1,18 @@
 import torch
 
+class LazilyInitializedLinear(torch.nn.Module):
+    def __init__(self, out_features):
+        super().__init__()
+        self.layer = None
+        self.out_features = out_features
+
+    def forward(self, x):
+        if not self.layer:
+            in_features = x.shape[1]
+            self.layer = torch.nn.Linear(in_features=in_features, out_features=self.out_features)
+        x = self.layer(x)
+        return x
+    
 #Reshape((1, 28, 28))
 #Reshape((28 * 28,))
 #Reshape((28 * 28))
